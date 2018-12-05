bfe81c4508250fa6df9d217096c2065e7cf213c0
Rahul Tiwari - SDK <rahul.tiwari@payu.in>
2018-12-05 15:54:03 +0530

    Update Hook.java

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -29,7 +29,7 @@ public class Hook extends BaseUtil{
 
 
         //Chrome driver
-        System.setProperty("webdriver.chrome.driver", "C:\\Libs\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "/Users/rahul.tiwari/Documents/automation/tutorial/driver/chromedriver");
         base.Driver = new ChromeDriver();
     }
 
